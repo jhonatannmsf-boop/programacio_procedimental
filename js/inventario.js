494c313bbd46a382d16de7122999c801ebe6982b
mauricio <jhonatannmsf@gmail.com>
2026-06-02 15:59:42 -0500

    modify_module_id_js

---
+++ js/inventario.js
@@ -0,0 +1,7 @@
+let producto = document.getElementById("nombre_producto").value;
+let precio = document.getElementById("precio").value;
+let codigo = document.getElementById("codigo").value;
+let marca = document.getElementById("marca").value;
+let cantidad = document.getElementById("cantidad").value;
+let categoria = document.getElementById("categoria").value;
+let unidad_medida = document.getElementById("unidad_medida").value;
---
+++ js/pedidos.js
@@ -0,0 +1,7 @@
+let nombre = document.getElementById("nombre_completo").value;
+let referencias = document.getElementById("referencias").value;
+let pedido = document.getElementById("pedido").value;
+let email = document.getElementById("email").value;
+let numero_celular = document.getElementById("numero_celular").value;
+let direccion = document.getElementById("direccion").value;
+let metodo_pago = document.getElementById("metodo_pago").value;
